0fbabe59781ffdf608e384dfd9005bb3abcd8368
ningk321 <62751724+ningk321@users.noreply.github.com>
2024-12-19 13:12:50 +0800

    Add files via upload

---
+++ nginx/exitnode.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# 从 GitHub 拉取最新的 exitnode.sh 脚本
+curl -sSL https://raw.githubusercontent.com/ningk321/Ray.Notes.Xui/refs/heads/main/nginx/exitnode.sh -o exitnode.sh
+
+# 给脚本赋予执行权限
+chmod +x exitnode.sh
+
